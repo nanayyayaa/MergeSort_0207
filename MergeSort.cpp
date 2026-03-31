862c4a9b2a4f79695177e0ecf988923b72b2d25c
nanayyayaa <jiejueejuju@gmail.com>
2026-03-31 10:56:43 +0700

    commit 5

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -40,4 +40,13 @@ void mergeSort(int low, int high)
     }
 
     int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high)
 }
