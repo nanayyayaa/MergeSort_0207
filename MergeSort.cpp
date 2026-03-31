557d25a7e260f2cb521b407ff0997172c34d869b
nanayyayaa <jiejueejuju@gmail.com>
2026-03-31 11:08:09 +0700

    commit 9

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -83,3 +83,12 @@ void mergeSort(int low, int high)
     }
 }
 
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
